9e65a1fb20ecd99ff61309c06d22aacd2f80f213
Amol Bhave <ammubhave@gmail.com>
2013-04-04 21:07:11 +0530

    Add templating, login

--- urls.py
+++ urls.py
@@ -7,6 +7,16 @@ admin.autodiscover()
 urlpatterns = patterns('',
     # Examples:
     url(r'^$', 'openshift.views.home', name='home'),
+    
+    url(r'^accounts/login$', 'django.contrib.auth.views.login'),
+    url(r'^accounts/logout$', 'django.contrib.auth.views.logout'),
+    url(r'^accounts/password_reset$', 'django.contrib.auth.views.password_reset'),
+    url(r'^accounts/password_reset_confirm/(?P<uidb36>[0-9A-Za-z]{1,13})-(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})$', 'django.contrib.auth.views.password_reset_confirm'),
+    url(r'^accounts/password_reset_done$', 'django.contrib.auth.views.password_reset_done'),
+    url(r'^accounts/password_reset_complete/$', 'django.contrib.auth.views.password_reset_complete', name='password_reset_complete'),
+    url(r'^accounts/password_change/$', 'django.contrib.auth.views.password_change', name='password_change'),
+    url(r'^accounts/password_change_done/$', 'django.contrib.auth.views.password_change_done', name='password_change_done'),
+    
     # url(r'^openshift/', include('openshift.foo.urls')),
 
     # Uncomment the admin/doc line below to enable admin documentation:
