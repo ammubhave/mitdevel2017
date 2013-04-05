bb8c8395560d119de6589a6a233c2b55a963b917
Amol Bhave <ammubhave@gmail.com>
2013-04-05 07:11:00 +0530

    update card

--- models.py
+++ models.py
@@ -2,8 +2,12 @@ from django.db import models
 from django.contrib.auth.models import User
 
 #class UserProfile(models.Model):
-#    user = models.OneToOneField(User)
+ #   user = models.OneToOneField(User)
+  #  tag_line = models.TextField(blank=True)
     # Other fields here
-    
     #accepted_eula = models.BooleanField()
     #favorite_animal = models.CharField(max_length=20, default="Dragons.")
+    
+#class SkillEntry(models.Model):
+ #   user = models.ForeignKey('User')
+  #  skill_name = models.TextField()
--- views/members.py
+++ views/members.py
@@ -1,7 +1,13 @@
 import os
 from django.shortcuts import render_to_response, render, get_object_or_404
 from django.contrib.auth.models import User
+#from openshift.models import UserProfile, SkillEntry
 
 def members(request):
     users = User.objects.all()
+    #for user in users:
+      #  skills = []
+     #   SkillEntry.objects.filter(user=user)
+        #skills.append(skill.name)
+        #user.skills = skills.join(',')
     return render(request, 'members.html', { 'users': users } )
