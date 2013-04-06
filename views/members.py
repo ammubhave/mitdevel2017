8e77707f08986cc74945d7e877d114a73e328cb9
Amol Bhave <ammubhave@gmail.com>
2013-04-06 13:03:41 +0530

    add filter by skill

--- views/members.py
+++ views/members.py
@@ -1,13 +1,22 @@
 import os
 from django.shortcuts import render_to_response, render, get_object_or_404
 from django.contrib.auth.models import User
-#from openshift.models import UserProfile, SkillEntry
+from openshift.models import SkillEntry
 
 def members(request):
     users = User.objects.all().order_by('first_name')
+    skills = set(SkillEntry.objects.all().values_list('skill_name', flat=True))
+    import random
+    skills = random.sample(skills, 10)
+    
+    if 'filter' in request.GET:
+        pfilter = request.GET['filter']
+        if pfilter is not None and pfilter != '':
+            users = SkillEntry.objects.filter(skill_name=str(pfilter).lower()).values_list('user', flat=True)
+            users = User.objects.filter(id__in = users)
     #for user in users:
       #  skills = []
      #   SkillEntry.objects.filter(user=user)
         #skills.append(skill.name)
         #user.skills = skills.join(',')
-    return render(request, 'members.html', { 'users': users } )
+    return render(request, 'members.html', { 'users': users, 'skills': skills } )
--- views/members_profile.py
+++ views/members_profile.py
@@ -10,12 +10,16 @@ def members_profile(request, username):
     for skill in skills:
         skill.label_class = 'default'
         if skill.proficiency == SkillEntry.STUDENT:
+            skill.proficiency_text = 'Student'
             skill.label_class = 'info'
         elif skill.proficiency == SkillEntry.BEGINNER:
+            skill.proficiency_text = 'Beginner'
             skill.label_class = 'success'
         elif skill.proficiency == SkillEntry.INTERMEDIATE:
+            skill.proficiency_text = 'Intermediate'
             skill.label_class = 'warning'
         elif skill.proficiency == SkillEntry.ADVANCED:
+            skill.proficiency_text = 'Advanced'
             skill.label_class = 'important'
     #for user in users:
       #  skills = []
